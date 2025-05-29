d010cfa9051feed119756b9565b9863d36000c26
julius <juliusterrence.duff@gmail.com>
2025-05-29 20:07:25 +0800

    changes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -22,6 +22,7 @@ return {
       lua = { "stylua" },
       python = { "isort", "black" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
+      blade = { "duster" }
     },
     -- Set default options
     default_format_opts = {
